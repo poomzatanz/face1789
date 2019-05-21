4d40117490bf303d414697bf42134faaf1c0b2cd
poomzatan <poomzatan@gmail.com>
2019-05-21 16:57:25 +0700

    sd

--- webhook.php
+++ webhook.php
@@ -1,11 +1,4 @@
 <?php
-$host="db4free.net";
-$user="poomzatan123456";
-$password="0811582889zX";
-$connect=mysqli_connect($host,$user,$password,"testdb1234567");
-mysqli_set_charset($connect,"UTF8");
-if($connect)
-{
 
 $hubVerifyToken = 'TOKEN123456abcd';
 $accessToken = "EAAEsPnhI9PsBAPaKEmS67h4zmEkrTZB6FZBZAc9RwIxmu62Qk7ZCPRZCZBvVZCZAc11taUEi2a8Tx5C9bapUSZCPylxaGfi76frnE7HJtlZBKqzmBgE7Go4sK0HPDjp8xmSvjAWVa85UMF6JuGlOVszOevipT66SRJf97dxZAdOC8SdMwZDZD";
@@ -18,6 +11,13 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$host="db4free.net";
+$user="poomzatan123456";
+$password="0811582889zX";
+$connect=mysqli_connect($host,$user,$password,"testdb1234567");
+mysqli_set_charset($connect,"UTF8");
+if($connect)
+{
 
 $sqltext = "INSERT INTO `idFace` (`id`, `idface`) VALUES (NULL, '$senderId');";
 	$qury = mysqli_query($connect,$sqltext);
@@ -29,20 +29,31 @@ $sqltext = "INSERT INTO `idFace` (`id`, `idface`) VALUES (NULL, '$senderId');";
 
 if($messageText == "hello") {
     $answer = "Hello ".$senderId." ";
+
+    $response = [
+      'recipient' => [ 'id' => '2396068373784859' ],
+      'message' => [ 'text' => $answer ]
+  ];
+  addFace($accessToken,$response)
 }
 else{
   $answer = "I don't understand. Ask me 'hi'.";
-}
-$response = [
+  $response = [
     'recipient' => [ 'id' => '2396068373784859' ],
     'message' => [ 'text' => $answer ]
 ];
-$ch = curl_init('https://graph.facebook.com/v3.3/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-curl_exec($ch);
-curl_close($ch);
+addFace($accessToken,$response)
+}
+
 }else{
 
 }
+function addFace($accessToken,$response){
+  $ch = curl_init('https://graph.facebook.com/v3.3/me/messages?access_token='.$accessToken);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+  curl_exec($ch);
+  curl_close($ch);
+}
+
