5d455c846453bdfe93c2486b94bb24305bbfce44
poomzatan <poomzatan@gmail.com>
2019-05-22 11:02:55 +0700

    sd

--- webhook.php
+++ webhook.php
@@ -14,37 +14,43 @@ $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 $response = null;
 //set Message
 if($messageText == "hi") {
-    $answer = "Hello";
-}elseif($messageText == "more") {  
+    $answer = "Hello1";
+    $response = [
+      'recipient' => [ 'id' => $senderId ],
+      'message' => $answer 
+    ];
+}if($messageText == "blog"){
   $answer = ["attachment"=>[
-      "type"=>"template",
-      "payload"=>[
-        "template_type"=>"button",
-        "text"=>"What do you want to do next?",
-        "buttons"=>[
-          [
-            "type"=>"web_url",
-            "url"=>"https://petersapparel.parseapp.com",
-            "title"=>"Show Website"
-          ],
-          [
-            "type"=>"postback",
-            "title"=>"Start Chatting",
-            "payload"=>"USER_DEFINED_PAYLOAD"
-          ]
-        ]
-      ]
-      ]];
-      $response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => $answer
-];
-}
-//send message to facebook bot
-$response = [
-    'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
-];
+   "type"=>"template",
+   "payload"=>[
+     "template_type"=>"generic",
+     "elements"=>[
+       [
+         "title"=>"Welcome to Peter\'s Hats",
+         "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+         "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+         "subtitle"=>"We\'ve got the right hat for everyone.",
+         "buttons"=>[
+           [
+             "type"=>"web_url",
+             "url"=>"https://petersfancybrownhats.com",
+             "title"=>"View Website"
+           ],
+           [
+             "type"=>"postback",
+             "title"=>"Start Chatting",
+             "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
+           ]              
+         ]
+       ]
+     ]
+   ]
+ ]];
+  $response = [
+ 'recipient' => [ 'id' => $senderId ],
+ 'message' => $answer 
+];}
+
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
