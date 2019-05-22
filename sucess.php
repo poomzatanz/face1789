ebd724b12218000aee159e44a0897826eba2a66d
poomzatan <poomzatan@gmail.com>
2019-05-22 13:44:25 +0700

    sd

--- sucess.php
+++ sucess.php
@@ -1,23 +1,25 @@
 
 <?php 
-"message":{
-  "attachment":{
-    "type":"template",
-    "payload":{
-      "template_type":"button",
-      "text":"What do you want to do next?",
-      "buttons":[
-        {
-          "type":"web_url",
-          "url":"https://www.messenger.com",
-          "title":"Visit Messenger"
-        },
-        {
-          ...
-        },
-        {...}
-      ]
-    }
-  }
-}
+ curl -X POST -H "Content-Type: application/json" -d '{
+     "recipient":{
+       "id":"2396068373784859"
+     },
+     "message":{
+       "attachment":{
+         "type":"template",
+         "payload":{
+           "template_type":"button",
+           "text":"What do you want to do next?",
+           "buttons":[
+             {
+               "type":"web_url",
+               "url":"https://www.anime-sugoi.com",
+               "title":"Visit Messenger"
+             }
+           ]
+         }
+       }
+     }
+   }' "https://graph.facebook.com/v2.6/me/messages?access_token=EAAEsPnhI9PsBAPaKEmS67h4zmEkrTZB6FZBZAc9RwIxmu62Qk7ZCPRZCZBvVZCZAc11taUEi2a8Tx5C9bapUSZCPylxaGfi76frnE7HJtlZBKqzmBgE7Go4sK0HPDjp8xmSvjAWVa85UMF6JuGlOVszOevipT66SRJf97dxZAdOC8SdMwZDZD"
+  
 ?>
--- webhook.php
+++ webhook.php
@@ -8,68 +8,62 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   echo $_REQUEST['hub_challenge'];
   exit;
 }
+$host="db4free.net";
+$user="poomzatan123456";
+$password="0811582889zX";
+$connect=mysqli_connect($host,$user,$password,"testdb1234567");
+mysqli_set_charset($connect,"UTF8");
+if($connect)
+{
 $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
-$response = null;
-//set Message
-if($messageText == "hi") {
-    $answer = "Hello";
-}elseif($messageText == "more") {  
-  $answer = ["attachment"=>[  
-    "type"=>"template", 
-   "payload"=> [  
-     "template_type"=>"button", 
-     "text"=>"What do you want to do next?", 
-    "buttons"=>[
-        [
-          "type"=>"web_url",
-          "url"=>"https://www.messenger.com",
-          "title"=>"Show Website"
-        ]
-      ]
-    ]
-    ]];
-}elseif ($messageText=="test") {
-  $answer = ["attachment"=>[
-    "type"=>"template",
-    "payload"=>[
-      "template_type"=>"generic",
-      "elements"=>[
-        [
-          "title"=>"Welcome to Peter\'s Hats",
-          "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
-          "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
-          "subtitle"=>"We\'ve got the right hat for everyone.",
-          "buttons"=>[
-            [
-              "type"=>"web_url",
-              "url"=>"https://petersfancybrownhats.com",
-              "title"=>"View Website"
-            ],
-            [
-              "type"=>"postback",
-              "title"=>"Start Chatting",
-              "payload"=>"DEVELOPER_DEFINED_PAYLOAD"
-            ]              
+
+$sqltext = "INSERT INTO `idFace` (`id`, `idface`) VALUES (NULL, '$senderId');";
+	$qury = mysqli_query($connect,$sqltext);
+	if($qury){
+               echo"<h1>ชื่อของคุณได้เก็บเข้าระบบแล้วครับ</h1>";
+               echo "<script type='text/javascript'>window.close();</script>";
+  }	
+  
+  $sqltext1 = "SELECT * FROM `Learn` WHERE input = '".$messageText."'";
+  $qury1 = mysqli_query($connect,$sqltext1);
+    $result=mysqli_fetch_array($qury1,MYSQLI_ASSOC);
+
+if($messageText == "hello") {
+    $answer = "Hello ".$senderId." ";
+}
+elseif ($result) {
+    $answer = $result['out'];
+}
+else{
+  $answer = [
+    'attachment' => [
+      'type' => 'template',
+      'payload'=> [
+        'template_type' => 'button',
+        'text' => 'test',
+        'buttons' => [
+          [
+            'type' => 'web_url',
+            'url' => 'https://www.anime-sugoi.com',
+            'title' => 'test'
           ]
         ]
       ]
     ]
-  ]];
-
+  ];
 }
-
 $response = [
     'recipient' => [ 'id' => $senderId ],
     'message' => [ 'text' => $answer ]
 ];
-$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+$ch = curl_init('https://graph.facebook.com/v3.3/me/messages?access_token='.$accessToken);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-if(!empty($input)){
-$result = curl_exec($ch);
-}
+curl_exec($ch);
 curl_close($ch);
-  
+mysqli_close($connect);
+exit;
+}
