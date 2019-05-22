dc05610742a563f2f25a93bde783f698c67e1520
poomzatan <poomzatan@gmail.com>
2019-05-22 10:55:41 +0700

    sd

--- webhook.php
+++ webhook.php
@@ -9,55 +9,78 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
   exit;
 }
 $input = json_decode(file_get_contents('php://input'), true);
-$page_id = $input['entry'][0]['id'];
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = isset($input['entry'][0]['messaging'][0]['message']['text']) ? $input['entry'][0]['messaging'][0]['message']['text']: '' ;
-$postback = isset($input['entry'][0]['messaging'][0]['postback']['payload']) ? $input['entry'][0]['messaging'][0]['postback']['payload']: '' ;
-if($message || $postback) { 
-  
-  
-  if($message) {
-       // If Page receives Message, process the Message and prepare content to reply
-       $reply = 'Message received: ' . $message;
-  }
-  else {
-    
-    // If Page receives Postback, process the Postback and prepare content to reply
-      
-    switch($postback) {
-    
-      case 'DEVELOPER_DEFINED_PAYLOAD_FOR_HELP':
-        $reply = 'You clicked Help button';
-        break;
-        
-      case 'DEVELOPER_DEFINED_PAYLOAD_FOR_LATEST_POSTS':
-        $reply = 'You clicked Latest Post button';
-        break;
-    
-    
-    }
-  
-  }
-  
-  
-   $responseJSON = '{
-    "recipient":{
-      "id":"'.$sender.'"
-    },
-    "message": {
-            "text":"'. $reply .'"
-        }
-  }';
-  $access_token = 'your_page_access_token';
-  
-  //Graph API URL
-  $url = 'https://graph.facebook.com/v2.7/me/messages?access_token='.$accessToken;
-  // Using cURL to send a JSON POST data
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $responseJSON);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-  $result = curl_exec($ch);
-  curl_close($ch);
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$response = null;
+//set Message
+if($messageText == "hi") {
+    $answer = "Hello";
+}elseif($messageText == "today"){
+  $answer = ["attachment"=>[
+       "type"=>"template",
+       "payload"=>[
+         "template_type"=>"list",
+         "elements"=>[
+           [
+              "title"=> "Classic T-Shirt Collection",
+                     "image_url"=> "https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+                     "subtitle"=> "See all our colors",
+                     "default_action"=> [
+                         "type"=> "web_url",
+                         "url"=> "https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",                       
+                         "webview_height_ratio"=> "tall",
+                         // "messenger_extensions"=> true,
+                         // "fallback_url"=> "https://peterssendreceiveapp.ngrok.io/"
+                     ],
+             "buttons"=>[
+               [
+                 "type"=>"web_url",
+                 "url"=>"https://petersfancybrownhats.com",
+                 "title"=>"View Website"
+               ],
+             ]
+           ],
+             [
+             "title"=>"Welcome to Peter\'s Hats",
+             "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+             "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+             "subtitle"=>"We\'ve got the right hat for everyone.",
+             "buttons"=>[
+               [
+                 "type"=>"web_url",
+                 "url"=>"https://petersfancybrownhats.com",
+                 "title"=>"View Website"
+               ],
+             ]
+           ],
+             [
+             "title"=>"Welcome to Peter\'s Hats",
+             "item_url"=>"https://www.cloudways.com/blog/migrate-symfony-from-cpanel-to-cloud-hosting/",
+             "image_url"=>"https://www.cloudways.com/blog/wp-content/uploads/Migrating-Your-Symfony-Website-To-Cloudways-Banner.jpg",
+             "subtitle"=>"We\'ve got the right hat for everyone.",
+             "buttons"=>[
+               [
+                 "type"=>"web_url",
+                 "url"=>"https://petersfancybrownhats.com",
+                 "title"=>"View Website"
+               ],
+             ]
+           ]
+         ]
+       ]
+     ]];
+//send message to facebook bot
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+if(!empty($input)){
+$result = curl_exec($ch);
+}
+curl_close($ch);
   
 }
